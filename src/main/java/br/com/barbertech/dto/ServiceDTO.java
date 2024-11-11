89feea4b035e70a6fc6f3874ab4bcd5b35628781
Leonardo Mauricio <leonardo.mauricio@sulamerica.com.br>
2024-11-11 12:37:15 -0300

    Adicionado testes unitarios dos controllers de barber, service e Auth

--- src/main/java/br/com/barbertech/dto/ServiceDTO.java
+++ src/main/java/br/com/barbertech/dto/ServiceDTO.java
@@ -2,13 +2,17 @@ package br.com.barbertech.dto;
 
 import br.com.barbertech.enums.GenderEnum;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServiceDTO {
 
     private Long id;
